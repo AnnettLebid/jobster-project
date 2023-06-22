8f6136819303321e0d3bd67a93923a84092437c6
Anna Lebid <annalez16@gmail.com>
2023-06-22 22:49:55 +0300

    initial commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
