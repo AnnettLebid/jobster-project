928943f47739bea279bff19f64f337fb4c24b268
Anna Lebid <annalez16@gmail.com>
2023-06-23 12:15:05 +0300

    add custom errors

---
+++ server/errors/index.ts
@@ -0,0 +1,4 @@
+export { CustomAPIError } from "./custom-api";
+export { UnauthenticatedError } from "./unauthenticated";
+export { BadRequest as BadRequestError } from "./bad-request";
+export { NotFoundError } from "./not-found";
