6b0f8721f316971175c333286c9550995848ab10
Anna Lebid <annalez16@gmail.com>
2023-06-23 10:24:39 +0300

    add typescript

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { connectDB } from "./db/connect.js";
+import { connectDB } from "./db/connect";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/auth");
+
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(`mongodb+srv://${process.env.MONGO_URI}`);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
