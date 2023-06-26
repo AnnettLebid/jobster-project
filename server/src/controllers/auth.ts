177a003ea29e6ac3c4df46c4e44cab7acbd67db0
Anna Lebid <annalez16@gmail.com>
2023-06-26 23:18:13 +0300

    add updateUser in auth controller

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { ExpressRequestInterface } from "../types/expressRequest.interface.js";
 import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
@@ -35,6 +36,39 @@ export const login = async (req: Request, res: Response) => {
   }
 
   const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+      email: user.email,
+      token,
+    },
+  });
+};
+
+export const updateUser = async (req: ExpressRequestInterface, res: Response) => {
+  const { email, name, lastName, location } = req.body;
+
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide email, name, lastName and location");
+  }
+
+  const user = await User.findOne({ _id: req.userId });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createJWT();
+
   res.status(StatusCodes.OK).json({
     user: {
       name: user.name,
