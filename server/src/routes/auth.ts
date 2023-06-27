b9951ad5901b4813025d46320c866bf2c9f4422f
Anna Lebid <annalez16@gmail.com>
2023-06-28 00:39:09 +0300

    add apiLimiter to register and login

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import rateLimiter from "express-rate-limit";
 import { login, register, updateUser } from "../controllers/auth.js";
 import { authenticateUser } from "../middleware/authentication.js";
 import { testUserLimitations } from "../middleware/testUser.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 router.patch("/updateUser", authenticateUser, testUserLimitations, updateUser);
 
 export default router;
