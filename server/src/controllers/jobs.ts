bf4a29ef3b52a4e488ca41e0488d38b022ec9a47
Anna Lebid <annalez16@gmail.com>
2023-06-27 08:36:10 +0300

    add updateJob and deleteJob

--- server/src/controllers/jobs.ts
+++ server/src/controllers/jobs.ts
@@ -18,3 +18,37 @@ export const createJob = async (req: ExpressRequestInterface, res: Response) => 
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json(job);
 };
+
+export const updateJob = async (req: ExpressRequestInterface, res: Response) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company and position fields cannot be empty");
+  }
+
+  const job = await Job.findByIdAndUpdate({ _id: id, createdBy: req.userId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
+
+export const deleteJob = async (req: ExpressRequestInterface, res: Response) => {
+  const { id } = req.params;
+
+  const job = await Job.findOneAndRemove({
+    _id: id,
+    createdBy: req.userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${id}`);
+  }
+  res.status(StatusCodes.OK).send();
+};
--- server/src/routes/jobs.ts
+++ server/src/routes/jobs.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { createJob, getJob } from "../controllers/jobs.js";
+import { createJob, getJob, updateJob, deleteJob } from "../controllers/jobs.js";
 
 const router = express.Router();
 
 router.route("/").post(createJob);
-router.route("/:id").get(getJob);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
 export default router;
