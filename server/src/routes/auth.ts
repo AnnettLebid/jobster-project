d45f5de9d77f0175606badaa850f52bf46d94f04
Anna Lebid <annalez16@gmail.com>
2023-06-28 00:33:35 +0300

    add testUserLimitations middleware to limit test user actions

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { login, register, updateUser } from "../controllers/auth.js";
-import {authenticateUser} from "../middleware/authentication.js";
+import { authenticateUser } from "../middleware/authentication.js";
+import { testUserLimitations } from "../middleware/testUser.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.patch("/updateUser", authenticateUser, updateUser);
+router.patch("/updateUser", authenticateUser, testUserLimitations, updateUser);
 
 export default router;
--- server/src/routes/jobs.ts
+++ server/src/routes/jobs.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createJob, getJob, updateJob, deleteJob, getAllJobs } from "../controllers/jobs.js";
+import { testUserLimitations } from "../middleware/testUser.js";
 
 const router = express.Router();
 
-router.route("/").post(createJob).get(getAllJobs);
-router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
+router.route("/").post(testUserLimitations, createJob).get(getAllJobs);
+router.route("/:id").get(getJob).patch(testUserLimitations, updateJob).delete(testUserLimitations, deleteJob);
 
 export default router;
