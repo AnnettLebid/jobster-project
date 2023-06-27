1ab01249b1c2d5a421e89517c749930e7f8dba6f
Anna Lebid <annalez16@gmail.com>
2023-06-27 14:52:55 +0300

    update getAllJobs with request query options

--- server/src/controllers/jobs.ts
+++ server/src/controllers/jobs.ts
@@ -5,8 +5,50 @@ import { ExpressRequestInterface } from "../types/expressRequest.interface.js";
 import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 export const getAllJobs = async (req: ExpressRequestInterface, res: Response) => {
-  const jobs = await Job.find({ createdBy: req.userId });
-  res.status(StatusCodes.OK).json({ jobs });
+  const { search, status, jobType, sort } = req.query;
+  const queryObject: any = {
+    createdBy: req.userId,
+  };
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  let result = Job.find(queryObject);
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  const jobs = await result;
+
+  res.status(StatusCodes.OK).send({ jobs, totalJobs, numOfPages });
 };
 
 export const getJob = async (req: ExpressRequestInterface, res: Response) => {
