61ce2cb3b222ade640b65a04e0cb29a5b41b549d
Anna Lebid <annalez16@gmail.com>
2023-07-09 21:40:07 +0300

    fix  __dirname for  ES module scope

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import "dotenv/config";
 import "express-async-errors";
 import { connectDB } from "./db/connect.js";
@@ -12,6 +13,9 @@ import { errorHandlerMiddleware } from "./middleware/error-handler.js";
 const app = express();
 
 app.set("trust proxy", 1);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
