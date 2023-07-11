c34f722a356d1f8d68f1d9ebce5e4092888131d3
Anna Lebid <annalez16@gmail.com>
2023-07-11 22:31:15 +0300

    convert App and index to .ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,10 +6,10 @@ import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <Provider store={store}>
-    <App tab="home" />
+    <App />
   </Provider>
 );
 
