3646e36e3d148bc7a40099c9115c13b02ce3b87f
Anna Lebid <annalez16@gmail.com>
2023-07-12 12:03:11 +0300

    update types

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
 import { clearStore } from "../features/user/userSlice";
 import { getUserFromLocalStorage } from "./localStorage";
 
@@ -8,17 +8,20 @@ const customFetch = axios.create({
 
 //we can add the interceptors to custom fetch (headers will be added automatically)
 //or to add auth headers or we can send authorization headers with each request
-customFetch.interceptors.request.use((config) => {
+customFetch.interceptors.request.use((config: AxiosRequestConfig<any>) => {
   const user = getUserFromLocalStorage();
-  if (user) {
-    config.headers["Authorization"] = `Bearer ${user.token}`;
+  if (user && config.headers) {
+    (config.headers as AxiosHeaders).set(
+      "Authorization",
+      `Bearer ${user.token}`
+    );
   }
   return config;
 });
 
-export const checkForUnauthorizedResponse = (error, thunkAPI) => {
+export const checkForUnauthorizedResponse = (error: any, thunkAPI: any) => {
   if (error.response.status === 401) {
-    thunkAPI.dispatch(clearStore());
+    thunkAPI.dispatch(clearStore(thunkAPI));
     return thunkAPI.rejectWithValue("Unauthorized! Logging out..");
   }
   return thunkAPI.rejectWithValue(error.response.data.msg);
