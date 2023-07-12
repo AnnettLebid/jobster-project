69811be768f6b1278b96a1d713a93bd0944a16a9
Anna Lebid <annalez16@gmail.com>
2023-07-12 13:32:19 +0300

    convert to ts

--- client/src/pages/Dashboard/Stats.tsx
+++ client/src/pages/Dashboard/Stats.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { showStats } from "../../features/allJobs/allJobsSlice";
 import { StatsContainer, ChartsContainer } from "../../components";
+import { RootState, useAppThunkDispatch } from "../../store";
 
 const Stats = () => {
-  const dispatch = useDispatch();
-  const { monthlyApplications } = useSelector((store) => store.allJobs);
+  const dispatch = useAppThunkDispatch();
+  const { monthlyApplications } = useSelector(
+    (store: RootState) => store.allJobs
+  );
 
   useEffect(() => {
     dispatch(showStats());
