2112b904fc6f801a5f688bff51caa2a296e8629d
Anna Lebid <annalez16@gmail.com>
2025-10-22 09:27:36 +0300

    fix: remove deprecated Mongoose connection options

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export const connectDB = (url: string) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   });
 };
